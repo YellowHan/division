6cd9ae87da0917baeaffe0518eea2167868d4504
YellowHan <89592727+YellowHan@users.noreply.github.com>
2022-03-22 18:07:16 +0900

    Add files via upload

---
+++ div3-2/index.js
@@ -0,0 +1,28 @@
+const now = new Date();
+const dayOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
+const today = `${now.getFullYear()}년 ${
+  now.getMonth() + 1
+}월 ${now.getDate()}일 ${dayOfWeek[now.getDay()]}요일 `;
+document.querySelector("h4").innerText = today;
+// Random Numbers
+const wrapper = document.querySelector(".wrapper");
+for (let i = 1; i <= 120; i += 5) {
+  wrapper.innerHTML += `
+<div class="underImg">
+<p class="leftNumber">
+  <span class="num${i}"></span>
+  <span class="num${i + 1}"></span>
+</p>
+<p class="rightNumber">
+  <span class="num${i + 2}"></span>
+  <span class="num${i + 3}"></span>
+  <span class="num${i + 4}"></span>
+</p>
+</div>
+`;
+}
+
+for (let i = 1; i <= 120; i++) {
+  const random = Math.floor(Math.random() * 9) + 1;
+  document.querySelector(`.num${i}`).innerText = random;
+}
